0fe01f49465f1a079c5f6046b906efa13aa72a2f
ghomess <gomes.a.tiago@gmail.com>
2024-05-18 18:04:32 +0100

    React Native Enviornment setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar hidden={true} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
